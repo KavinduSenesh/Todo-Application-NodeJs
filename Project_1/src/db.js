fe5d3f1865348102647a07cf6a075991c942c111
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-29 22:23:08 +0530

    Refactor code style for consistency and add authentication routes

--- Project_1/src/db.js
+++ Project_1/src/db.js
@@ -1,14 +1,14 @@
-import { DatabaseSync } from 'node:sqlite'
-const db = new DatabaseSync(':memory:')
+import { DatabaseSync } from "node:sqlite";
+const db = new DatabaseSync(":memory:");
 
 // Execute SQL staements from strings
 db.exec(`
     CREATE TABLE users (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE
+        username TEXT UNIQUE,
         password TEXT
     )
-`)
+`);
 
 db.exec(`
     CREATE TABLE todos (
@@ -18,6 +18,6 @@ db.exec(`
         completed BOOLEAN DEFAULT 0,
         FOREIGN KEY (user_id) REFERENCES user (id)
     )
-`)
+`);
 
-export default db
+export default db;
--- Project_1/src/server.js
+++ Project_1/src/server.js
@@ -1,7 +1,8 @@
-import express from 'express';
-import { console } from 'inspector';
-import path, {dirname} from 'path';
-import {fileURLToPath} from 'url';
+import express from "express";
+import { console } from "inspector";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,15 +15,18 @@ const __dirname = dirname(__filename);
 // Middleware
 app.use(express.json());
 // Serves the HTML file from the public folder
-// Tells express to serve static files from the public folder as assests / 
+// Tells express to serve static files from the public folder as assests /
 // file. Any requests for the css files will be resolved to the public folder
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // serving up the HTML file from the public folder
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+//Routes
+app.use("/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
