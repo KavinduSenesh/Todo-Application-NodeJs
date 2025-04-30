3331c6c5f1f2c10d80f08d10dfcf5d85e8cf82d6
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-30 10:57:59 +0530

    Implement todo routes for CRUD operations and integrate with server

--- Project_1/src/server.js
+++ Project_1/src/server.js
@@ -3,6 +3,7 @@ import { console } from "inspector";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from "./routes/todoRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -26,6 +27,7 @@ app.get("/", (req, res) => {
 
 //Routes
 app.use("/auth", authRoutes);
+app.use("/todos", todoRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
