913d10e580b45a2b02e2601c008fe7d12d1c7571
Kavindu Senesh <ksenesh5@gmail.com>
2025-04-30 19:11:58 +0530

    Implement authentication middleware and update todo routes to use userId for fetching todos

--- Project_1/src/routes/todoRoutes.js
+++ Project_1/src/routes/todoRoutes.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
   const getTodos = db.prepare("SELECT * FROM todos WHERE user_id = ?");
-  const todos = getTodos.all(req.user_id);
+  const todos = getTodos.all(req.userId);
   res.json(todos);
 });
 
